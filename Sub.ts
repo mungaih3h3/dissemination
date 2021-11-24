802cf3973f6620edb2acddc9ffc024bd5c6bcd91
Mungai <itsmungai@gmail.com>
2021-11-24 09:04:08 +0300

    pub sub

--- Sub.ts
+++ Sub.ts
@@ -1,9 +1,21 @@
-export default class Sub {
-  notify(event: string) {}
+import Pub from "./Pub";
+
+export default interface Sub {
+  notify: (event: string) => any;
 }
 
-export class Logger {
+export class Logger extends Pub implements Sub {
   notify(event: string) {
     console.log(`[${new Date()}]: `, event);
+    this.publish(event + " (nested)");
+  }
+}
+
+export class Analytics extends Pub implements Sub {
+  count: Map<string, number> = new Map();
+  notify(event: string) {
+    this.count.set(event, (this.count.get(event) || 0) + 1);
+    console.log(`Analytics: ${event} count = ${this.count.get(event)}`);
+    this.publish(event);
   }
 }
--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
-import { Logger } from "./Sub";
+import { Analytics, Logger } from "./Sub";
 import Pub from "./Pub";
 
 const hub = new Pub();
 
 const sub1 = new Logger();
 const sub2 = new Logger();
+const sub3 = new Analytics();
 
+sub1.register(sub2);
+hub.register(sub3);
 hub.register(sub1);
-hub.register(sub2);
 
+hub.publish("call");
 hub.publish("call");
